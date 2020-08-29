2459142a55f63e75cd71362e5aabc6c8ee949f44
RoomMovie <roommovie0@gmail.com>
2020-08-28 21:56:50 -0500

    createmenu and view of task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ViewtaskComponent } from './components/viewtask/viewtask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ViewtaskComponent
   ],
   imports: [
     BrowserModule,
